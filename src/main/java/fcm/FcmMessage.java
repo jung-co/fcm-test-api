c25e5a617903a5e73cc4b033077c0100d270f51e
jungco <dal_a2@naver.com>
2021-12-16 09:43:11 +0900

    [#2] FcmMessage

--- src/main/java/fcm/FcmMessage.java
+++ src/main/java/fcm/FcmMessage.java
@@ -1,7 +1,7 @@
 package fcm;
 
 import com.google.auto.value.AutoValue.Builder;
-import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,4 +11,21 @@ import lombok.Getter;
 public class FcmMessage {
     private boolean validate_only;
     private Message message;
+    
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Message {
+        private Notification notification;
+        private String token;
+    }
+    
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Notification {
+        private String title;
+        private String body;
+        private String image;
+    }
 }
