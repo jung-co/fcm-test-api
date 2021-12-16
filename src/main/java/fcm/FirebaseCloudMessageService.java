e8ec95418f2bcd24ed050c6fdc209bf8ecbcc96d
jungco <dal_a2@naver.com>
2021-12-16 10:34:34 +0900

    [#4] CREATE HTTP POST Request

--- src/main/java/fcm/FirebaseCloudMessageService.java
+++ src/main/java/fcm/FirebaseCloudMessageService.java
@@ -3,9 +3,50 @@ package fcm;
 import java.io.IOException;
 import java.util.List;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.common.net.HttpHeaders;
+import lombok.RequiredArgsConstructor;
+import okhttp3.*;
 
+@Component
+@RequiredArgsConstructor
 public class FirebaseCloudMessageService {
+    
+    private final String API_URL = "https://fcm.googleapis.com/v1/projects/tourcash-13092/messages:send";
+    private final ObjectMapper objectMapper;
+    
+    public void sendMessageTo(String targetToken, String title, String body) throws IOException {
+        String message = makeMessage(targetToken, title, body);
+        
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+    }
+    
+    private String makeMessage(String targetToken, String title, String body) throws JsonProcessingException {
+        FcmMessage fcmMessage = FcmMessage.builder()
+                .message(FcmMessage.Message.builder()
+                        .token(targetToken)
+                        .notification(FcmMessage.Notification.builder()
+                                .title(title)
+                                .body(body)
+                                .image(null)
+                                .build()
+                            )
+                        .build()
+                    )
+                .valicaate_only(false)
+                .build();
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+    
     private String getAccessToken() throws IOException {
         String firebaseConfigPath = "firebase/service-account.json";
         
